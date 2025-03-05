ab0862fb1c18e343e4948be276af9789b7a2a9aa
Koduri <srinadh@ayu.health>
2025-03-05 12:12:57 +0530

    removed console

--- src/utils/delay.ts
+++ src/utils/delay.ts
@@ -1,9 +1,7 @@
 export const randomDelay = (min: number, max: number) => {
   return new Promise<void>((resolve) => {
-console.log('randomDelay');
     const delay = Math.random() * (max - min) + min;
     setTimeout(() => {
-console.log('delay');
       resolve();
     }, delay);
   });
